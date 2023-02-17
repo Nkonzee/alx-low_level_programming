89679d80b0e47f2839500d7bbb456918ca4ebea7
Nkonzee <nkondachollom@gmail.com>
2023-02-16 17:51:07 -0800

    ptints a line with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- A program that prints correct grammer with an out put of art
+ * Return: 0 (Runs without erros)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
