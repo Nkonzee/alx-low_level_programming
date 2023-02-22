32d6738b0953f31d3e188293cb4d379131ecbe12
Nkonzee <nkondachollom@gmail.com>
2023-02-21 16:21:33 -0800

    1-alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,3 @@
-#ifndef main_h
-#define main_h
-
-
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -16,5 +12,3 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 int add(int, int);
-
-#endif
