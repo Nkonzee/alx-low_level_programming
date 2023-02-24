78dc464187e1691c29d95b2cd6c0094dfb389384
Nkonzee <nkondachollom@gmail.com>
2023-02-23 20:36:52 -0800

    6-lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - entry point
+ *
+ * print_lines -  Prints lines
+ *@n: number of lines
+ * Return: void
+ */
+
+void print_line(int n)
+{
+
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
