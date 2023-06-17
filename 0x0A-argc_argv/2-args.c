22fa1fbc77f767c431cf89d9c6573089583e7653
nkonzee <nkondachollom@gmail.com>
2023-06-17 00:48:16 -0700

    All arguments should be printed, including the first one

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Program entry point
+ * @argc: Counts all arguments in a program
+ * @argv: Array that holds all arguments as string in a program
+ *
+ * Return: Return 0.
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	(void)argc;
+
+	for (i = 0; i <= argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
