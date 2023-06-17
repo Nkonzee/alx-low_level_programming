82a1ee1c172d6f735787d4ca2e30cddb9c40e63b
nkonzee <nkondachollom@gmail.com>
2023-06-17 00:27:17 -0700

    argument counter

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Argument counter
+ * @argv: Array that holds argument string
+ *
+ * Return: Return 0.
+ *
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
